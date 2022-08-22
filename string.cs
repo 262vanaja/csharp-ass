78a4d6a72751a8a764a246417ca24b8074b120d4
262vanaja <105485514+262vanaja@users.noreply.github.com>
2022-08-22 19:41:41 +0530

    Add files via upload

---
+++ string.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class @string
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(first_last("bujji"));
+            Console.WriteLine(first_last("sundaram"));
+            Console.WriteLine(first_last("vanu"));
+        }
+        public static string first_last(string ustr)
+        {
+            return ustr.Length > 1
+                ? ustr.Substring(ustr.Length - 1) + ustr.Substring(1, ustr.Length - 2) + ustr.Substring(0, 1) : ustr;
+        }
+    }
+}
---
+++ vowles.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class vowles
+    {
+        public static bool isVowel(char x)
+        {
+            x = char.ToUpper(x);
+            return (x == 'A' || x == 'E' || x == 'I' || x == 'O' || x == 'U');
+        }
+
+        public static int countVowels(string str)
+        {
+            int count = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (isVowel(str[i]))
+                {
+                    ++count;
+                }
+            }
+            return count;
+        }
+
+        public static void Main(string[] args)
+        {
+
+            string str = "bujji";
+
+            Console.WriteLine(countVowels(str));
+        }
+    }
+}
