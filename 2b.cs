1fb8a0ff6d509147dd6beb7f73ae80a6bf52c46e
262vanaja <105485514+262vanaja@users.noreply.github.com>
2022-08-26 09:04:15 +0530

    Add files via upload

---
+++ 2b.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class _2b
+    {
+        public static void swap(ref int a, ref int b)
+        {
+            int t = a;
+            a = b; b = t;
+            Console.WriteLine($"a={a},b={b}");
+        }
+
+
+        public static void Main()
+        {
+
+
+            int a = 5;
+            int b = 4;
+
+            Console.WriteLine($"beforw swap a={a},b={b}");
+
+            swap(ref a, ref b);
+            Console.WriteLine($"after  swap a={a},b={b}");
+        }
+
+    }
+}
